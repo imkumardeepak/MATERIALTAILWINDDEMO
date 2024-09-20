791dc4d5ca70f26a4863cbe551a45091f4c4aaa5
Deepak Kumar <deepakraznish@gmail.com>
2024-09-20 17:36:03 +0530

    materialtailwind

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
           <Option>Material Tailwind Svelte</Option>
         </Select>
       </div>
-      <Card className="w-full max-w-[26rem] shadow-xl">
+      <Card className="w-full max-w-[26rem] shadow-xl mt-2">
         <CardHeader floated={false} color="blue-gray">
           <img
             src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
